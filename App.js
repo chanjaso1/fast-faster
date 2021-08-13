318d6f72b5d04c319c9b368d0d8052d228c69460
Jason <chanjaso1@ecs.vuw.ac.nz>
2021-08-13 18:07:09 +1200

    Initiation implementation of tab navigation with icons. Each screen is defined but only with some text for differentiation.

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+function mealPlanScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Meal Planning!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function fastingScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Fasting!</Text>
+    </View>
+  );
+}
+
+function statsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Stats!</Text>
+    </View>
+  );
+}
+
+function settingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+         screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+
+            if(route.name == "Meal Plans"){
+                iconName = focused ? "ios-list-circle" : "ios-list-circle-outline";
+            }else if(route.name == "Fasting") {
+              iconName = focused ? "timer" : "timer-outline"; 
+            }else if(route.name == "Settings") {
+                iconName = focused ? "settings" : "cog-outline"; 
+            }else if(route.name == "Stats") {
+                iconName = focused ? "stats-chart" : "stats-chart-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarInactiveTintColor:'grey',
+          tabBarActiveTintColor:'black',
+        })}
+        >
+      
+        <Tab.Screen name="Meal Plans" component={mealPlanScreen} />
+        <Tab.Screen name="Fasting" component={fastingScreen} />
+        <Tab.Screen name="Stats" component={statsScreen} />
+        <Tab.Screen name="Settings" component={fastingScreen} />
+      
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
